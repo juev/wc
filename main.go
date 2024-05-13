6e729e0a01170fd878db6dafb8db27d6a6cdfacc
Evsyukov Denis <denis@evsyukov.org>
2024-05-13 11:11:27 +0300

    feat: draft for words

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"unicode"
 )
 
 func main() {
@@ -24,11 +25,11 @@ func main() {
 
 func run() error {
 	if len(files) == 0 {
-		c, s, l, err := processFile()
+		c, s, l, w, err := processFile()
 		if err != nil {
 			return err
 		}
-		fmt.Println(c, s, l)
+		print(c, s, l, w, "")
 		return nil
 	}
 
@@ -36,12 +37,13 @@ func run() error {
 		Count int64
 		Size  int64
 		Lines int64
+		Words int64
 		File  string
 	}
 	var FF = make([]ff, 0, len(files))
-	var totalCount, totalSize, totalLines int64
+	var totalCount, totalSize, totalLines, totalWords int64
 	for i := range files {
-		c, s, l, err := processFile(files[i])
+		c, s, l, w, err := processFile(files[i])
 		if err != nil {
 			return err
 		}
@@ -49,53 +51,37 @@ func run() error {
 			Count: c,
 			Size:  s,
 			Lines: l,
+			Words: w,
 			File:  files[i],
 		})
 		totalCount += c
 		totalSize += s
 		totalLines += l
+		totalWords += w
 	}
 
 	for i := range FF {
-		if chars {
-			fmt.Print(FF[i].Count, "\t")
-
-		}
-		if bytes {
-			fmt.Print(FF[i].Size, "\t")
-		}
-		if lines {
-			fmt.Print(FF[i].Lines, "\t")
-		}
-		fmt.Println(FF[i].File)
+		print(FF[i].Count, FF[i].Size, FF[i].Lines, FF[i].Words, FF[i].File)
 	}
-	if len(FF) > 1 {
-		if chars {
-			fmt.Print(totalCount, "\t")
 
-		}
-		if bytes {
-			fmt.Print(totalSize, "\t")
-		}
-		if lines {
-			fmt.Print(totalLines)
-		}
-		fmt.Println()
+	if len(FF) > 1 {
+		print(totalCount, totalSize, totalLines, totalWords, "total")
 	}
 
 	return nil
 }
 
-func processFile(fileName ...string) (count, size, lines int64, err error) {
+func processFile(fileName ...string) (count, size, lines, words int64, err error) {
 	file := os.Stdin
 	if len(fileName) != 0 {
 		file, err = os.Open(fileName[0])
 		if err != nil {
-			panic(err)
+			return 0, 0, 0, 0, nil
 		}
 	}
 	defer file.Close()
 
+	var prev rune
 	reader := bufio.NewReader(file)
 	for {
 		r, s, err := reader.ReadRune()
@@ -103,14 +89,35 @@ func processFile(fileName ...string) (count, size, lines int64, err error) {
 			break
 		}
 		if err != nil {
-			return 0, 0, 0, err
+			return 0, 0, 0, 0, err
 		}
 		if r == '\n' {
 			lines++
 		}
+		if !unicode.IsSpace(r) && unicode.IsSpace(prev) {
+			words++
+		}
+		prev = r
 		count++
 		size += int64(s)
 	}
 
-	return count, size, lines, nil
+	return count, size, lines, words, nil
+}
+
+func print(c, s, l, w int64, file string) {
+	if chars {
+		fmt.Print(c, "\t")
+
+	}
+	if bytes {
+		fmt.Print(s, "\t")
+	}
+	if lines {
+		fmt.Print(l, "\t")
+	}
+	if words {
+		fmt.Print(w, "\t")
+	}
+	fmt.Println(file)
 }
